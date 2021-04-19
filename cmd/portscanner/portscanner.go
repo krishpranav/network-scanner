bf6d554d141418a20a2ad27f13c9400fd41dee97
krishpranav <krisna.pranav@gmail.com>
2021-04-19 11:33:56 +0530

    added banner

---
+++ cmd/portscanner/portscanner.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+
+	"github.com/krishpranav/network-scanner"
+)
+
+func PrintBanner() {
+
+	fmt.Println("\n +----------------+")
+	fmt.Println(" | Go PortScanner |")
+	fmt.Println(" +----------------+")
+}
